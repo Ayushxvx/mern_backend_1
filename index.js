1f69a3e64d26a92053761864dac63413ca4a461e
Ayush Verma <132675966+Ayushxvx@users.noreply.github.com>
2025-09-06 15:50:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(urlencoded({extended:true}));
 app.use(helmet());
-app.use(cors({origin:'https://thenet.netlify.app/',credentials:true}))
+app.use(cors({origin:'https://thenet.netlify.app',credentials:true}))
 app.use(morgan("dev"));
 
 app.use("/api/auth",urouter);
